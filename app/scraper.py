f6c7d71f2812c75b69bc136353b56a60d983837f
cristy <cristi.gutzu@gmail.com>
2025-11-13 15:41:14 +0100

    advanced args for create_scraper

--- app/scraper.py
+++ app/scraper.py
@@ -24,22 +24,10 @@ class ScraperService:
                 # Challenge handling
                 interpreter='js2py',  # Best compatibility for v3 challenges
                 delay=5,  # Extra time for complex challenges
-
                 # Stealth mode
                 enable_stealth=True,
-                stealth_options={
-                    'min_delay': 2.0,
-                    'max_delay': 6.0,
-                    'human_like_delays': True,
-                    'randomize_headers': True,
-                    'browser_quirks': True
-                },
-
                 # Browser emulation
                 browser='chrome',
-
-                # Debug mode
-                debug=True
             )
         return self.scraper
 
